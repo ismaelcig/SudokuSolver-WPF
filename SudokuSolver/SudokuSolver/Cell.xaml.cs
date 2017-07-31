87c2aae0733c7e2e9c51f3059812ccf42e4cdf26
ismaelcig <Ismael.condeig@gmail.com>
2017-07-31 12:29:31 +0200

    Initial Commit
    Project Created, just the Sudoku Board

---
+++ SudokuSolver/SudokuSolver/Cell.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SudokuSolver
+{
+    /// <summary>
+    /// Lógica de interacción para Cell.xaml
+    /// </summary>
+    public partial class Cell : UserControl
+    {
+        public bool selected = false;
+        public Cell()
+        {
+            InitializeComponent();
+            lbl.Content = "";
+        }
+
+        public void SelectionChanged()
+        {
+            if (selected)
+            {
+                grid.Background = Brushes.Bisque;
+            }
+            else
+            {
+                grid.Background = Brushes.White;
+            }
+        }
+
+        public void setNum(int n)
+        {
+            lbl.Content = n.ToString();
+            //TODO: If the number is already on use, set background to red
+        }
+
+        private void lbl_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //Deselect other cells
+            MainWindow.UpdateSelection();
+            //Select this cell
+            selected = true;
+            MainWindow.selectedCell = this;
+            SelectionChanged();
+        }
+    }
+}
---
+++ SudokuSolver/SudokuSolver/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SudokuSolver
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static List<Cell> cells;
+        public static Cell selectedCell = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            cells = new List<Cell>();
+            foreach (Cell item in g.Children)
+            {
+                cells.Add(item);
+            }
+        }
+        public static void UpdateSelection()
+        {
+            foreach (Cell c in cells)
+            {
+                c.selected = false;
+                c.SelectionChanged();
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {//Sólo se pueden pulsar números
+            int keyVal = (int)e.Key;
+            int value = -1;
+            if (keyVal >= (int)Key.D0 && keyVal <= (int)Key.D9)
+            {
+                value = (int)e.Key - (int)Key.D0;
+            }
+            else if (keyVal >= (int)Key.NumPad0 && keyVal <= (int)Key.NumPad9)
+            {
+                value = (int)e.Key - (int)Key.NumPad0;
+            }
+            if (value > 0)
+            {
+                selectedCell.setNum(value);
+            }
+            
+            
+        }
+    }
+}
